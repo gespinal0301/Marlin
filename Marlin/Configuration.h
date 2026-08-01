8c20041a3203a8f6ca8c784a45362dd2998e3c09
Gabriel <gespinal0301@gmail.com>
2026-07-31 19:36:44 -0600

    Increase Z feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1372,7 +1372,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 20, 25 } // GE
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 50, 25 } // GE
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
