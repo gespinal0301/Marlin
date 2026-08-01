c8992aeab56bc6a3c5d397daf3439cc005f1f625
Gabriel <gespinal0301@gmail.com>
2026-08-01 15:08:05 -0600

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4515,7 +4515,7 @@
 #define MECHANICAL_GANTRY_CALIBRATION // GE
 #if ENABLED(MECHANICAL_GANTRY_CALIBRATION)
   #define GANTRY_CALIBRATION_CURRENT          600     // Default calibration current in ma
-  #define GANTRY_CALIBRATION_EXTRA_HEIGHT      10 // GE     // Extra distance in mm past Z_###_POS to move
+  #define GANTRY_CALIBRATION_EXTRA_HEIGHT      15 // GE     // Extra distance in mm past Z_###_POS to move
   #define GANTRY_CALIBRATION_FEEDRATE         500     // Feedrate for correction move
   //#define GANTRY_CALIBRATION_TO_MIN                 // Enable to calibrate Z in the MIN direction
 
